d6b49f29cec2d59d767c64469c572f01b329ebbc
zzh <1181132030@qq.com>
2020-03-05 12:18:05 +0800

    组员A编写

---
+++ src/main/java/com/team/house/housebackapi/controller/A.java
@@ -0,0 +1,12 @@
+package com.team.house.housebackapi.controller;
+
+/**
+ * @Author ZzHh
+ * @Classname A
+ * @Description TODO
+ * @Date: Created in 2020/3/5 12:16
+ * @Create By IntelliJ IDEA
+ **/
+
+public class A {
+}
